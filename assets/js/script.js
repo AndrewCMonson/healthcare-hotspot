31a1dd1fb84d118688a5bd1f5fc38f6db64cfb31
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-10-29 09:35:55 -0400

    expanded js file; we can edit/review together, just wanted to get some ideas on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 // this is a sample api fetch that returns 200 results based on a zipcode entered.
-
+// example of API url call
+//https://corsproxy.io/?https://npiregistry.cms.hhs.gov/api/?version=2.1& +postal_code + =$ +zipcode + &pretty=on&limit=10
 
 const getHealthInfo = (zipcode) => {
     const url = `https://corsproxy.io/?https://npiregistry.cms.hhs.gov/api/?version=2.1&postal_code=${zipcode}&pretty=on&limit=10`;
@@ -51,5 +52,39 @@ const compareZips = (userInput, response) => {
 
 }
 
+// splitting data we will need into defined variables for use later, feel free to edit
+// found these definitions here: https://npiregistry.cms.hhs.gov/help-api/json-conversion
 
+var providerName = basic.organization_name
+var providerAddressLine1 = addresses[1].address_1
+var providerAddressLine2 = addresses[1].address_2
+var providerAddressCity = addresses[1].city
+var providerAddressState = addresses[1].state
+var providerAddressZip = addresses[1].postal_code
+var providerPhoneNumber = addresses[0].telephone_number
 
+// building an outline of the function we may need to build a data array of each provider's info we want
+// we should be able to pull multiple pieces of data within 1 function (??)
+
+function parseProviderData (providerContactInfo){
+    // an object with the info we want from the API, each as a property
+    const {
+        // may need to expand this to first & last depending on accuracy of test pull
+        Name: providerName,
+        // not sure if I can do this
+        Address: providerAddressLine1 + providerAddressLine2
+        City: providerAddressCity,
+        State: providerAddressState,
+        Zipcode: providerAddressZip,
+        Phone: providerPhoneNumber,
+    } = providerContactInfo
+
+    return {
+    providerName,
+    providerAddressLine1,
+    providerAddressLine1,
+    providerAddressCity,
+    providerAddressState,
+    providerAddressZip,
+}
+}
