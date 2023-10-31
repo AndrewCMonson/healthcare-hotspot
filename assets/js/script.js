36b3c1fd62ba04f61a3bba2a52e3c894a3f31892
Andrew Monson <andrewmonsondev@gmail.com>
2023-10-30 20:16:33 -0400

    completed buildProviders function and started display function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,79 +12,112 @@ const getHealthInfo = (zipcode) => {
         // returns api response, converts to JSON then stores JSON data into variable
         .then(response => response.json())
         // .then(responseJson => testDisplay(responseJson))
-        .then(responseJson => console.log(responseJson))
+        // .then(responseJson => console.log(responseJson.results))
+        .then(responseJson => buildProviders(responseJson.results))
         // .then(responseJson => compareZips(1740903558, responseJson))        
 }
 
+
+getHealthInfo(29680)
+
+
+// This function takes in an API response as an argument and builds an array of objects (providers) with the API information
+const buildProviders = (data) => {
+    const providerArr = [];
+
+    for(let i = 0; i < data.length; i++){
+        const currentProvider = data[i];
+        // console.log(data[i])
+
+        const providerInfo = {
+            providerFirstName: currentProvider.basic.first_name,
+            providerLastName: currentProvider.basic.last_name,
+            providerAddressOne: currentProvider.addresses[1].address_1,
+            providerCity: currentProvider.addresses[1].city,
+            providerState: currentProvider.addresses[1].state,
+            providerZip:currentProvider.addresses[1].postal_code,
+            providerTelephone: currentProvider.addresses[1].telephone_number,
+        }
+
+        providerArr.push(providerInfo);
+    }
+    // console.log(providerArr);
+
+    providerArr.forEach(displayProviders);
+}
+
+const displayProviders = (arr) => {
+
+    const testDiv = document.getElementById('test-div');
+    // console.log(arr.providerFirstName)
+
+    if(arr.providerFirstName){
+        console.log(arr.providerFirstName)
+        const newDiv = document.createElement('div');
+        testDiv.append(newDiv);
+        newDiv.textContent = arr.providerFirstName
+
+    }
+    // const displayDiv = document.getElementById('display-div');
+    // const newDiv = document.createElement('div');
+    // displayDiv.appendChild(newDiv);
+    // newDiv.textContent = rr[currentProviderInfo].providerFirstName
+    
+
+    // const newDiv = document.createElement('div');
+    // displayDiv.appendChile(newDiv);
+    // newDiv.textContent = arr.providerFirstName
+
+}
+
+
+
 // write functions that ingest API response and populate to the DOM
 
-const testDisplay = (response) => {
-    // drill down into the data we want to display
-    // addresses
-    // providers name
-    // provider phone
-    const testPara = document.getElementById('test-para');
+// const testDisplay = (response) => {
+//     // drill down into the data we want to display
+//     // addresses
+//     // providers name
+//     // provider phone
+//     const testPara = document.getElementById('test-para');
 
-    testPara.textContent = response.results[0].addresses[0].postal_code
-}
+//     testPara.textContent = response.results[0].addresses[0].postal_code
+// }
 
-getHealthInfo(29680);
+// getHealthInfo(29680);
 
-/**
- * input a zipcode
- * if zipcode === api results zip code
- * return the information attached to that array
- */
+// /**
+//  * input a zipcode
+//  * if zipcode === api results zip code
+//  * return the information attached to that array
+//  */
 
-// have to compare an input zipcode to zipcodes stored in api response
-const compareZips = (userInput, response) => {
-    let savedZipCodes;
-    const responseResults = response.results[0];
+// // have to compare an input zipcode to zipcodes stored in api response
+// const compareZips = (userInput, response) => {
+//     let savedZipCodes;
+//     const responseResults = response.results[0];
 
 
-    for(let i = 0; i < responseResults.length; i++){
-        if(userInput === responseResults[i].number){
-            savedZipCodes = responseResults[i].number
-        }
-    }
+//     for(let i = 0; i < responseResults.length; i++){
+//         if(userInput === responseResults[i].number){
+//             savedZipCodes = responseResults[i].number
+//         }
+//     }
     
-    console.log(savedZipCodes);
+//     console.log(savedZipCodes);
 
-}
+// }
 
-// splitting data we will need into defined variables for use later, feel free to edit
-// found these definitions here: https://npiregistry.cms.hhs.gov/help-api/json-conversion
+// // splitting data we will need into defined variables for use later, feel free to edit
+// // found these definitions here: https://npiregistry.cms.hhs.gov/help-api/json-conversion
 
-var providerName = basic.organization_name
-var providerAddressLine1 = addresses[1].address_1
-var providerAddressLine2 = addresses[1].address_2
-var providerAddressCity = addresses[1].city
-var providerAddressState = addresses[1].state
-var providerAddressZip = addresses[1].postal_code
-var providerPhoneNumber = addresses[0].telephone_number
+// var providerName = results[0].basic.organization_name
+// var providerAddressLine1 = addresses[1].address_1
+// var providerAddressLine2 = addresses[1].address_2
+// var providerAddressCity = addresses[1].city
+// var providerAddressState = addresses[1].state
+// var providerAddressZip = addresses[1].postal_code
+// var providerPhoneNumber = addresses[0].telephone_number
 
-// building an outline of the function we may need to build a data array of each provider's info we want
-// we should be able to pull multiple pieces of data within 1 function (??)
+// console.log(`${providerAddressLine1} ${providerAddressLine2}`);
 
-function parseProviderData (providerContactInfo){
-    // an object with the info we want from the API, each as a property
-    const {
-        // may need to expand this to first & last depending on accuracy of test pull
-        Name: providerName,
-        // not sure if I can do this
-        Address: providerAddressLine1 + providerAddressLine2
-        City: providerAddressCity,
-        State: providerAddressState,
-        Zipcode: providerAddressZip,
-        Phone: providerPhoneNumber,
-    } = providerContactInfo
-
-    return {
-    providerName,
-    providerAddressLine1,
-    providerAddressLine1,
-    providerAddressCity,
-    providerAddressState,
-    providerAddressZip,
-}
-}
