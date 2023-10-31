7e657899869eb428c8c22358317f20f95e567d90
Andrew Monson <andrewmonsondev@gmail.com>
2023-10-30 21:59:29 -0400

    display providers function completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,7 @@ const buildProviders = (data) => {
         // console.log(data[i])
 
         const providerInfo = {
+            providerOrgName: currentProvider.basic.organization_name,
             providerFirstName: currentProvider.basic.first_name,
             providerLastName: currentProvider.basic.last_name,
             providerAddressOne: currentProvider.addresses[1].address_1,
@@ -46,103 +47,66 @@ const buildProviders = (data) => {
     providerArr.forEach(displayProviders);
 }
 
+// This function takes in an array of objects and publishes select information to the DOM based on if the provided arr contains a provider first name or not
+// If there is no first name, it uses the org name and publishes the rest of the information accordingly
+// This is used by a forEach method within the object array constructor function (buildProvider)
 const displayProviders = (arr) => {
-
+    console.log(arr);
     const testDiv = document.getElementById('test-div');
+    const nameDiv = document.createElement('div');
+    const addressDiv = document.createElement('div');
+    const telephoneDiv = document.createElement('div');
+
+    // nameDiv.className = 'column'
     // console.log(arr.providerFirstName)
 
     if(arr.providerFirstName){
         console.log(arr.providerFirstName)
-        const nameDiv = document.createElement('div');
         testDiv.append(nameDiv);
         nameDiv.textContent = `${arr.providerFirstName} ${arr.providerLastName}`
-        // const lastNameDiv = document.createElement('div');
-        // testDiv.append(lastNameDiv);
-        // lastNameDiv.textContent = arr.providerLastName
-    }if(!arr.providerFirstName){
+        if(arr.providerAddressOne){
+            console.log(arr.providerAddressOne);
+            nameDiv.append(addressDiv);
+            addressDiv.textContent = `${arr.providerAddressOne} ${arr.providerCity} ${arr.providerState} ${arr.providerZip}`;
+        }else {
+            nameDiv.append(addressDiv);
+            addressDiv.textContent = `No Address Provided`;
+        }
+        if(arr.providerTelephone){
+            console.log(arr.providerTelephone)
+            nameDiv.append(telephoneDiv);
+            telephoneDiv.textContent = `${arr.providerTelephone}`;
+        }else {
+            console.log(arr.providerTelephone)
+            
+            nameDiv.append(telephoneDiv);
+            telephoneDiv.textContent = `No Phone Number Provided`;
+        }
+    } 
+    else {
+        console.log(arr.providerOrgName)
         const nameDiv = document.createElement('div');
         testDiv.append(nameDiv);
-        nameDiv.textContent = `No name provided`;
-    }if(arr.providerAddressOne){
-        console.log(arr.providerAddressOne)
-        const addressDiv = document.createElement('div');
-        testDiv.append(addressDiv);
-        addressDiv.textContent = `${arr.providerAddressOne} ${arr.providerCity} ${arr.providerState} ${arr.providerZip}`;
-    }if(!arr.providerAddressOne){
-        const addressDiv = document.createElement('div');
-        testDiv.append(addressDiv);
-        addressDiv.textContent = `No Address Provided`;
-    }if(arr.providerAddressOne){
-        // console.log(arr.providerTelephone)
-        const telephoneDiv = document.createElement('div');
-        testDiv.append(telephoneDiv);
-        telephoneDiv.textContent = `${arr.providerTelephone}`;
-    }if(!arr.providerAddressOne){
-        // console.log(arr.providerTelephone)
-        const telephoneDiv = document.createElement('div');
-        testDiv.append(telephoneDiv);
-        telephoneDiv.textContent = `No Phone Number Provided`;
+        nameDiv.textContent = `${arr.providerOrgName}`;
+        if(arr.providerAddressOne){
+            console.log(arr.providerAddressOne);
+            nameDiv.append(addressDiv);
+            addressDiv.textContent = `${arr.providerAddressOne} ${arr.providerCity} ${arr.providerState} ${arr.providerZip}`;
+        }else {
+            nameDiv.append(addressDiv);
+            addressDiv.textContent = `No Address Provided`;
+        }
+        if(arr.providerTelephone){
+            console.log(arr.providerTelephone)
+            nameDiv.append(telephoneDiv);
+            telephoneDiv.textContent = `${arr.providerTelephone}`;
+        }else {
+            console.log(arr.providerTelephone)
+            
+            nameDiv.append(telephoneDiv);
+            telephoneDiv.textContent = `No Phone Number Provided`;
+        }
     }
-    // const displayDiv = document.getElementById('display-div');
-    // const newDiv = document.createElement('div');
-    // displayDiv.appendChild(newDiv);
-    // newDiv.textContent = rr[currentProviderInfo].providerFirstName
-    
-
-    // const newDiv = document.createElement('div');
-    // displayDiv.appendChile(newDiv);
-    // newDiv.textContent = arr.providerFirstName
-
 }
 
-
-
-// write functions that ingest API response and populate to the DOM
-
-// const testDisplay = (response) => {
-//     // drill down into the data we want to display
-//     // addresses
-//     // providers name
-//     // provider phone
-//     const testPara = document.getElementById('test-para');
-
-//     testPara.textContent = response.results[0].addresses[0].postal_code
-// }
-
-// getHealthInfo(29680);
-
-// /**
-//  * input a zipcode
-//  * if zipcode === api results zip code
-//  * return the information attached to that array
-//  */
-
-// // have to compare an input zipcode to zipcodes stored in api response
-// const compareZips = (userInput, response) => {
-//     let savedZipCodes;
-//     const responseResults = response.results[0];
-
-
-//     for(let i = 0; i < responseResults.length; i++){
-//         if(userInput === responseResults[i].number){
-//             savedZipCodes = responseResults[i].number
-//         }
-//     }
-    
-//     console.log(savedZipCodes);
-
-// }
-
-// // splitting data we will need into defined variables for use later, feel free to edit
-// // found these definitions here: https://npiregistry.cms.hhs.gov/help-api/json-conversion
-
-// var providerName = results[0].basic.organization_name
-// var providerAddressLine1 = addresses[1].address_1
-// var providerAddressLine2 = addresses[1].address_2
-// var providerAddressCity = addresses[1].city
-// var providerAddressState = addresses[1].state
-// var providerAddressZip = addresses[1].postal_code
-// var providerPhoneNumber = addresses[0].telephone_number
-
-// console.log(`${providerAddressLine1} ${providerAddressLine2}`);
 
